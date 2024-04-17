24f5e95a00c6521879e8cc46faddd1a6590793d0
jebi2420 <jebi2420@gmail.com>
2024-04-17 17:48:36 +0900

    [add] index.js router 적용

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient()
 
 root.render(
   <QueryClientProvider client = {queryClient}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right'/>
   </QueryClientProvider>
 );
