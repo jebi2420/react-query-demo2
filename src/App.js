d14deb733399a4d911cba852eaea1d1da0a24e1d
jebi2420 <jebi2420@gmail.com>
2024-04-17 18:01:44 +0900

    [add] app 페이지 구성, routers 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <nav style={{backgroundColor: "beige", padding: "20px"}}>
+        <Link to="/" style={{ marginRight: "10px"}}>
+          Homepage
+        </Link>
+        <Link to="/react-query">React Query</Link>
+      </nav>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/react-query" element={<ReactQueryPage />}/>
+    </Routes>
     </div>
   );
 }
